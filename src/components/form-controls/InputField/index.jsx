27a622a90772b233e356048f4b35992c8a4d8d52
Phuoc Thanh <thanhlp18@gmail.com>
2023-04-29 18:17:33 +0700

    form fields update

--- src/components/form-controls/InputField/index.jsx
+++ src/components/form-controls/InputField/index.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { TextField } from '@material-ui/core';
+import { Controller } from 'react-hook-form';
 
 InputField.propTypes = {
+    form: PropTypes.object.isRequired,
+    name: PropTypes.string.isRequired,
 
+    label: PropTypes.string,
+    disabled: PropTypes.bool
 };
 
 function InputField(props) {
+    const { form, name, label, disabled } = props;
+
     return (
-        <div>
-            <TextField fullWidth />
-        </div>
+        <Controller
+            name={name}
+            control={form.control}
+            as={TextField}
+
+            fullWidth
+            label={label}
+            disabled={disabled}
+        />
+
     );
 }
 
