fa937501c2267d44082f487e81c3928535f08f8b
Phuoc Thanh <thanhlp18@gmail.com>
2023-04-29 18:53:36 +0700

    Learn react hook form

--- src/features/Todo/components/TodoForm/index.jsx
+++ src/features/Todo/components/TodoForm/index.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import InputField from 'components/form-controls/InputField';
 import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+
 
 TodoForm.propTypes = {
     onSubmit: PropTypes.func,
 };
 
 function TodoForm(props) {
+    // Dinh nghia thang schema
+    const schema = yup.object().shape({
+        title: yup.string().required('Please enter title!'),
+    });
+
+    // Chen thang resolver vao useForm va su dung thang schema
     const form = useForm({
         defaultValues: {
             title: ''
-        }
+        },
+        resolver: yupResolver(schema)
     })
 
     const handleSubmit = (values) => {
