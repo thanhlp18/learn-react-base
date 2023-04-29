cd05c9a80001c4ae6b870ec4615793737b1dc218
Phuoc Thanh <thanhlp18@gmail.com>
2023-04-29 16:51:11 +0700

    test revert

--- src/components/form-controls/InputField/index.jsx
+++ src/components/form-controls/InputField/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { TextField } from '@material-ui/core';
+import { propagateErrors } from 'yup/lib/util/runValidations';
 
 InputField.propTypes = {
-
+    form: PropTypes.object.isRequired,
+    name: propagateErrors.string
 };
 
 function InputField(props) {
@@ -13,5 +15,6 @@ function InputField(props) {
         </div>
     );
 }
+fasdfa
 
 export default InputField;
